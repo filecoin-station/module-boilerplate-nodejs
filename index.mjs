c1d0f26596f6ec7febb31b4cf9d1ee9d5f87bf2a
Miroslav Bajtoš <oss@bajtos.net>
2023-01-24 10:51:36 +0100

    Add more structure & explanation

--- index.mjs
+++ index.mjs
@@ -1,9 +1,21 @@
 import http from 'node:http'
 import { promisify } from 'node:util'
 
+//
+// 1. Load the configuration
+//
+
 // Optional configuration
 // eslint-disable-next-line no-unused-vars
 const { FIL_WALLET_ADDRESS, ROOT_DIR } = process.env
+
+//
+// 2. Bootstrap your Station Module. 
+//
+// You can perform asynchronous operations here. Station does not
+// consider the module as running until you print the `API:` line
+// (see below).
+//
 
 const server = http.createServer((req, res) => {
   res.end(JSON.stringify({
@@ -15,8 +27,16 @@ const server = http.createServer((req, res) => {
 const listen = promisify(server.listen.bind(server))
 await listen()
 
+//
+// 3. Let the Station know we are ready
+//
+
 // Station users won't see this
 console.log(`API: http://localhost:${server.address().port}`)
+
+//
+// 4. Optionally, you can log additional info at any time
+//
 
 // Station users will see this
 console.log('INFO: Connected to network')
